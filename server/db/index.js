a79aeb52a65c5869b5ae5316f4550d303e2f4bd1
AnjiNai <anjali.nainani@gmail.com>
2021-08-03 17:26:31 -0400

    Feature In Process: All Genies Page - Backend Route Added (buggie), Redux Store Updated Until Reducer

--- server/db/index.js
+++ server/db/index.js
@@ -1,29 +1,25 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
-const Genie = require('./models/Genie')
-const Order = require('./models/Order')
+const User = require("./models/User");
+const Genie = require("./models/Genie");
+const Order = require("./models/Order");
 // const { BelongsToMany } = require('sequelize/types')
-
-
 
 //associations could go here!
 
-Genie.belongsToMany(Order, {through:'Orderline'});
-Order.belongsToMany(Genie, {through:'Orderline'});
+Genie.belongsToMany(Order, { through: "Orderline" });
+Order.belongsToMany(Genie, { through: "Orderline" });
 
 Order.belongsTo(User);
 User.hasMany(Order);
-
-
 
 module.exports = {
   db,
   models: {
     User,
-    Genie, 
-    Order
+    Genie,
+    Order,
   },
-}
+};
