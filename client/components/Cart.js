6a9ed369dd8fd931bca82909439c576f17aa215a
nelasunitha <nelasunitha@gmail.com>
2021-08-06 11:28:19 -0700

    editing cart copmleted,should fix bugs after merging

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,119 +2,134 @@ import axios from "axios";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { deleteCart, updateCart,addToCart } from "../store/cart"
-import CartContainer from "./CartContainer";
+import { deleteCart, updateCart, addToCart } from "../store/cart";
+// import CartContainer from "./CartContainer";
 
 class Cart extends Component {
   constructor(props) {
-    console.log('props', props);
+    console.log("props", props);
     super(props);
     this.state = {
-     cart:[]
+      cart: [],
     };
-    this.handleAdd =this.handleAdd.bind(this)
-    this.handleSubtract = this.handleSubtract(this)
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubtract = this.handleSubtract(this);
     this.handleDelete = this.handleDelete.bind(this);
   }
   componentDidMount() {
-    const guestCart = JSON.parse(localStorage.getItem('guestCart'))
-    this.setState({cart: guestCart})
-    if((this.props.match.params.userId)>0) {
-      this.props.getCart(this.props.match.params.userId)
+    const guestCart = JSON.parse(localStorage.getItem("guestCart"));
+    this.setState({ cart: guestCart });
+    if (this.props.match.params.userId > 0) {
+      this.props.getCart(this.props.match.params.userId);
     }
-
   }
- componentDidUpdate(prevProps) {
-   if(prevProps.userId !== thi.props.userId) {
-     this.props.getCart(this.props.userId)
-   }
- }
+  componentDidUpdate(prevProps) {
+    if (prevProps.userId !== this.props.userId) {
+      this.props.getCart(this.props.userId);
+    }
+  }
   handleAdd(evt, genie) {
-    this.setState(state => {
-      const genies= this.state.genies
-      const genieInCart = false;
-      genies.forEach(item => {
-        if(item.id === genie.id) {
+    this.setState((state) => {
+      const genies = state.genies;
+      let genieInCart = false;
+      genies.forEach((item) => {
+        if (item.id === genie.id) {
           genieInCart = true;
-          item.inventory++
+          item.inventory++;
         }
-      })
-      if(!genieInCart) {
-        genieInCart.push({...genies,inventory:1})
+      });
+      if (!genieInCart) {
+        genieInCart.push({ ...genies, inventory: 1 });
       }
-       localStorage.setItem('guestCart', JSON.stringify(prevCart))
+      localStorage.setItem("guestCart", JSON.stringify(genieInCart));
+    });
+  }
 
-    })
+  handleSubtract(evt, genie) {
+    this.setState((state) => {
+      const genies = state.genies;
+      let genieInCart = false;
+      genies.forEach((item) => {
+        if (item.id === genie.id) {
+          genieInCart = true;
+          item.inventory--;
+        }
+      });
+      if (!genieInCart) {
+        genieInCart.push({ ...genies, inventory: 1 });
+      }
+      localStorage.setItem("guestCart", JSON.stringify(genieInCart));
+    });
   }
 
 
-  // handleSubtract(evt) {
-  //   evt.persist();
-  //     const { data} =  axios.get(`api/genies/${evt.target.id}`)
-  //     this.props.decrement([this.props.cart.userId,data,qtyType])
-
-  //     let prevCart = JSON.parse(localStorage.getItem('guestCart'));
-  //     let truthy;
-  //     let id = evt.target.id
-  //     if(!prevCart) {
-  //       prevCart = [];
-  //       localStorage.setItem('guestCart', JSON.stringify(prevCart))
-  //     }else {
-  //       prevCart.map(genie => {
-  //         if(genie.id === id) {
-  //           genie.inventory--;
-  //         }
-  //       })
-  //     }
-
-  //   }
-
-
-
-   handleDelete(evt,genie) {
-
-    this.setState(state => {
-      let updatedCart = state.genies.filter(ele => ele.id !== genie.id);
-    localStorage.setItem('guestCart', updatedCart);
-    return {updatedCart}
-  })
-}
+  handleDelete(evt, genie) {
+    this.setState((state) => {
+      let updatedCart = state.genies.filter((ele) => ele.id !== genie.id);
+      localStorage.setItem("guestCart", updatedCart);
+      return { updatedCart };
+    });
+  }
 
   render() {
+    const { genies } = this.props;
     return (
-      <div className = 'cart'>
+      <div>
+        <div className="alert">
+          {genies.length === 0 ? (
+            " Cart is Empty"
+          ) : (
+            <div> You have {genies.length} products,</div>
+          )}
+          {genies.length > 0 && (
+            <div>
+              <ul>
+                {genies.map((genie) => (
+                  <li className="genie" key={genie.Id || genie.genieId}>
+                    <b>{genie.name}</b>X{genie.inventory}
+                    <button
+                      className="Remove"
+                      onClick={(e) => this.props.handleDelete(e, genie)}
+                    >
+                      X
+                    </button>
+                    <div>
+                      {genies.inventory<= 10}
+                        <button
+                      className="Add"
+                      onClick={(e) => this.props.handleAdd(e, genie)}
+                    >
+                      +
+                    </button>
 
+                    </div>
 
-         {/* ADD products should come here */} handleAdd = {this.handleAdd}
-            <CartContainer  genies = {this.state.genies} handleSubtract = {this.handleSubtract}/>
-            <h3>{genie.price}</h3>
-            <div>Remove Item</div>
-              {this.state.edit && genie.orderline0 && (<button type = "button" onClick ={()=> this.handleSubtract(genie.id,1,genie)}></button>
-              )}
-
-            <div>Add Item  </div>
-             {this.state.edit && genie.orderline?quantity>0 && (<button type = "button" onClick ={()=> this.handleAdd(genie.id,1,genie)}></button>
-            )}
-
-
-        )}
-
-    </div>
-    )
-
+                    <button
+                      className="Less"
+                      onClick={(e) => this.props.handleSubtract(e, genie)}
+                    >
+                      -
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    );
   }
-
 }
 const mapStateToProps = (state) => {
   return {
     cart: state.cart,
   };
 };
-const mapDispatchToProps = (dispatch, { history }) => ({
-  addToCart:(genieId) => dispatch(addToCart(genieId)),
-  updatedQty: (genieId, currQty, genie) =>
-  dispatch(updateCart(genieId, currQty, genie)),
-  deleteFromCart:(id) => dispatch(deleteCart(id))
+const mapDispatchToProps = (dispatch) => ({
+  addToCart: (genieId) => dispatch(addToCart(genieId)),
+  updatedQty: (genieId, quantity) =>
+    dispatch(updateCart(genieId, quantity)),
+  deleteFromCart: (id) => dispatch(deleteCart(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
