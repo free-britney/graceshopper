5ebc6015a20a8ee71e357697564675cc39e9fe3b
AnjiNai <anjali.nainani@gmail.com>
2021-08-07 17:55:58 -0400

    Adding to cart using local storage and displaying that data on the orders page - css and buttons need to be completed

--- client/components/Orders.js
+++ client/components/Orders.js
@@ -5,7 +5,11 @@ import { fetchOrder } from "../store/orders";
 class Orders extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    // AN Note: Displaying Cart on Orders Page:
+    // If local storage cart exists, this.cart = local storage cart.
+    // If it doesn't, set this.cart to an empty array.
+    this.cart = JSON.parse(window.localStorage.getItem("Cart")) || [];
+    this.state = { cart: this.cart };
     console.log(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -16,16 +20,65 @@ class Orders extends Component {
     // console.log("these are the props!" , this.props);
     this.props.fetchOrder(this.props.order.id);
   }
-  
+
   handleSubmit(evt) {
     evt.preventDefault();
   }
-  
+
   render() {
-    // console.log(this.props);
+    const { cart } = this.state;
+    console.log(cart);
     return (
-    <div>Orders</div>
-    )
+      <div className="container">
+        <div className="d-sm-flex justify-content-between align-items-center">
+          <h2>Cart</h2>
+        </div>
+        <div />
+
+        <div className="container">
+          <div className="row row-cols-1 row-cols-md-1 g-4">
+            {cart.length === 0 ? (
+              <div className="card">
+                <div className="card-body">
+                  <h6>**There are no items in your cart.**</h6>
+                </div>
+              </div>
+            ) : (
+              cart.map((genie) => (
+                <div key={genie.id} className="col">
+                  <img
+                    src={genie.imageURL}
+                    className="img-fluid rounded-start"
+                  />
+                  <div className="card bg-warning border-dark text-light">
+                    <div className="card-body text-center">
+                      <div className="h1">
+                        <h4 className="card-title">{genie.name}</h4>
+
+                        <h6 className="card-text">
+                          <span className="text-primary">Description: </span>
+                          {genie.description}
+                        </h6>
+                        <h6 className="card-text">
+                          <span className="text-primary">Price: </span>
+                          {genie.price}
+                        </h6>
+                        <h6 className="card-text">
+                          <span className="text-primary">Wish Quantity: </span>
+                          {genie.wishQty}
+                          <div />
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+        <div className="text-center">End of List</div>
+      </div>
+    );
   }
 }
 
