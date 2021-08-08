9f6f8efafae1dd4f36313354f202948968286c58
nelasunitha <nelasunitha@gmail.com>
2021-08-08 00:08:30 -0700

    some Bug

--- client/store/orders.js
+++ client/store/orders.js
@@ -4,8 +4,6 @@ const initialState = {};
 //action type
 const ADD_GENIE_TO_ORDER = "ADD_GENIE_TO_ORDER";
 const GET_ORDER = "GET_ORDER";
-const DELETE_QUANTIY = "DELETE_QUANTIY";
-const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 
 //action creator
 export const addGenieToOrder = (genie) => {
@@ -19,19 +17,6 @@ export const getOrder = (order) => {
   return {
     type: GET_ORDER,
     order,
-  };
-};
-const deleteQuantity = (item) => {
-  return {
-    type: DELETE_QUANTIY,
-    item,
-  };
-};
-
-const deleteFromCart = (genie) => {
-  return {
-    type: REMOVE_FROM_CART,
-    genie,
   };
 };
 
@@ -59,34 +44,6 @@ export const fetchOrder = (orderId) => {
     }
   };
 };
-export const deleteGenie = (id,name) => {
-  return async (dispatch) => {
-    try {
-      const {data}= await axios.delete(`/api/orders/genie/${id}`);
-      dispatch(deleteFromCart(data));
-    } catch (error) {
-      console.error(error);
-    }
-  };
-}
-export const editQuantity = (genieObj,history)=> {
-  return async(dispatch) => {
-    try {
-      const userId = {
-        id: genieObj[0]
-      }
-      const qty = {
-        type: genieObj[2]
-      }
-      const {data} = await axios.post(`/api/orders/`, [userId, genieObj[1],qty])
-      dispatch(deleteQuantity(data));
-      history.push(`/`)
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-
 
 //reducer
 export default function ordersReducer(state = initialState, action) {
@@ -95,21 +52,6 @@ export default function ordersReducer(state = initialState, action) {
       return action.genie;
     case GET_ORDER:
       return action.order;
-    case DELETE_QUANTIY:
-        if(state.length !== 0)
-          return state.genies.map((genie) => {
-            if (genie.id === action.genieId && genie.wishQty>1) {
-              genie.wishQty = genie.wishQty-1;
-              return genie;
-            }return genie;
-          })
-    case REMOVE_FROM_CART:
-          const cartAfterDelete = state.genies.filter((genie)=>{
-            return genie.id !== action.genieId
-          });
-          const newState = {...state}
-          newState.genies = cartAfterDelete;
-           return newState
     default:
       return state;
   }
