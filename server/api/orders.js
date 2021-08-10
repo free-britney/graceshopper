e2880ea373b513032d743c94ab33327a194218a6
Farha Lina <farhalin@gmail.com>
2021-08-10 17:48:27 -0400

    added api orders put route

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Order } = require("../db");
+const User = require("../db/models/User");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -10,13 +11,52 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-router.post("/", async (req, res, next) => {
-  try {
-    const newOrder = await Order.create(req.body);
-    res.json(newOrder);
+// router.post("/", async (req, res, next) => {
+//   try {
+//     if(req.body.userId){
+//       const newOrder = await Order.create(req.body);
+//       const currentUser = await User.findByPk(req.body.userId);
+//       currentUser.addOrder(newOrder);
+//     res.json(newOrder);
+//     } else {
+//       const newOrder = await Order.create(req.body);
+//       res.json(newOrder);
+//     }
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+
+router.put("/", async (req, res, next) => {
+  try { 
+    if(req.body.userId){  // if there is a user, and the next line, we will see if the user has a cart
+      const hasCart = await Order.findOne({
+        where: {
+          userId: req.body.userId,
+          orderStatus: "pending"
+        }
+      }) 
+      if(hasCart){
+        res.json(await hasCart.update(req.body))
+        console.log(hasCart);
+      } else {
+        const newOrder = await Order.create(req.body);
+        const currentUser = await User.findByPk(req.body.userId);
+        currentUser.addOrder(newOrder);
+        res.json(newOrder);
+      }
+    } else {
+      //guest logic here
+      const newOrder = await Order.create(req.body);
+      res.json(newOrder);
+    }
+    // moving all logic here
+    // const editOrder = await Order.findByPk(req.body.orderId);
+    // res.json(await editOrder.update(req.body))
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
 module.exports = router;
+
